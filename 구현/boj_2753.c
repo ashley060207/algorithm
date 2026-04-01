b57f3ea500d2728ef9ac39f4bef2ba77264e74a1
SeoYoung <choiashley0207@gmail.com>
2026-04-01 23:35:35 +0900

    Add leap year determination program

---
+++ 구현/boj_2753.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+
+    int year;
+    scanf("%d", &year);
+
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+        puts("1");
+    } else {
+        puts("0");
+    }
+
+    return 0;
+}
