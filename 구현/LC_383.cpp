2349604fd346f178a4a2e9da99af035b0fa07554
SeoYoung <choiashley0207@gmail.com>
2026-04-27 22:15:23 +0900

    Implement canConstruct function for ransom note problem

---
+++ 구현/LC_383.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        
+        int arr[26] {0};
+
+        for(int i = 0; i < magazine.size(); i++){
+            arr[magazine[i] - 'a']++;
+        }
+
+        for(int i = 0; i < ransomNote.size(); i++){
+            if(--arr[ransomNote[i] - 'a'] < 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
