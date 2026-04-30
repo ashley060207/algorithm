b2eaf40f8132505d66e3a357bb5c07a424900af7
SeoYoung <choiashley0207@gmail.com>
2026-04-30 22:00:01 +0900

    Implement function to merge two integers as strings

---
+++ 구현/pgm_더크게합치기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int a, int b) {
+    string ab = to_string(a) + to_string(b);
+    string ba = to_string(b) + to_string(a);
+    
+    if(stoi(ab) > stoi(ba)){
+        return stoi(ab);
+    }
+    return stoi(ba);
+}
